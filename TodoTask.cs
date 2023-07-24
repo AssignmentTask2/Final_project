bc4129b16e830da160e9d07a6082279e972fdaf1
AssignmentTask2 <140198871+AssignmentTask2@users.noreply.github.com>
2023-07-24 21:31:31 +0530

    Add files via upload

---
+++ TodoTask.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorApp
+{
+    public class TodoTask
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string DueDate { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+
+
+
+
+}
